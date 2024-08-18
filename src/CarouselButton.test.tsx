4811bf2ea92efdf46e19b067dc961c0e390ce33d
bechernie <bechernie@gmail.com>
2024-08-18 18:31:29 +0200

    CarouselButton second iteration

--- src/CarouselButton.test.tsx
+++ src/CarouselButton.test.tsx
@@ -6,4 +6,22 @@ describe('CarouselButton', () => {
         render(<CarouselButton/>);
         expect(screen.getByRole("button")).toBeInTheDocument();
     });
+
+    it('should pass "children" through to the <button>', () => {
+        const text = 'Button text';
+        render(<CarouselButton>{text}</CarouselButton>);
+        expect(screen.getByRole('button')).toHaveTextContent('Button text');
+    });
+
+    it('should pass other props through to the <button>', () => {
+        const className = 'my-carousel-button';
+        const dataAction = 'prev';
+        render(
+            <CarouselButton className={className} data-action={dataAction}>
+                Button text
+            </CarouselButton>
+        );
+        expect(screen.getByRole('button')).toHaveClass('my-carousel-button');
+        expect(screen.getByRole('button')).toHaveAttribute('data-action', dataAction);
+    });
 });
--- src/CarouselButton.tsx
+++ src/CarouselButton.tsx
@@ -1,3 +1,9 @@
-const CarouselButton = () => <button/>;
+import {ComponentPropsWithRef} from "react";
+
+const CarouselButton = (props: ComponentPropsWithRef<'button'>) => {
+    return (
+        <button {...props}/>
+    );
+};
 
 export default CarouselButton;
