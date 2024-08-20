324a8fe1244d433f8b07522b6a371cc78b02d85e
bechernie <bechernie@gmail.com>
2024-08-20 08:43:53 +0200

    styled lint setup

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,21 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react({
+            babel: {
+                plugins: [
+                    [
+                        "babel-plugin-styled-components",
+                        {
+                            displayName: true,
+                            fileName: true,
+                        }
+                    ]
+                ]
+            }
+        })
+    ],
     // @ts-expect-error test is missing from the type definition
     test: {
         globals: true,
