75800c21a7a8e960be025986ee44336feff7767e
bechernie <bechernie@gmail.com>
2024-08-24 11:27:39 +0200

    prevent updates from resetting the auto-advance timer

--- src/Carousel.test.tsx
+++ src/Carousel.test.tsx
@@ -127,4 +127,29 @@ describe('with auto-advance', () => {
         act(() => vi.advanceTimersByTime(autoAdvanceInterval));
         expect(img).toHaveAttribute("src", slides[2].imgUrl);
     });
+
+    it('should not reset the auto-advance timer on re-render', () => {
+        const autoAdvanceInterval = 5_000;
+        const {rerender} = render(<Carousel slides={slides} autoAdvanceInterval={autoAdvanceInterval}/>);
+        const img = screen.getByRole("img");
+        expect(img).toHaveAttribute("src", slides[0].imgUrl);
+        act(() => vi.advanceTimersByTime(autoAdvanceInterval - 1));
+        rerender(<Carousel slides={slides} autoAdvanceInterval={autoAdvanceInterval}/>);
+        act(() => vi.advanceTimersByTime(1));
+        expect(img).toHaveAttribute("src", slides[1].imgUrl);
+    });
+
+    it('should not reset the timer on irrelevant props changes', () => {
+        const autoAdvanceInterval = 5_000;
+        const CarouselParent = () => <Carousel slides={slides}
+                                               onSlideIndexChange={vi.fn()}
+                                               autoAdvanceInterval={autoAdvanceInterval}/>;
+        const {rerender} = render(<CarouselParent/>);
+        const img = screen.getByRole("img");
+        expect(img).toHaveAttribute("src", slides[0].imgUrl);
+        act(() => vi.advanceTimersByTime(autoAdvanceInterval - 1));
+        rerender(<CarouselParent/>);
+        act(() => vi.advanceTimersByTime(1));
+        expect(img).toHaveAttribute("src", slides[1].imgUrl);
+    });
 });
--- src/useSlideIndex.tsx
+++ src/useSlideIndex.tsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useCallback, useRef, useState} from "react";
 import {useTimeout} from "./useTimeout.tsx";
 
 const decrement = (length: number) => (i: number) => (i + length - 1) % length;
@@ -7,6 +7,8 @@ const increment = (length: number) => (i: number) => (i + 1) % length;
 export const useSlideIndex = (slides?: unknown[], slideIndexProp?: number, onSlideIndexChange?: (newSlideIndex: number) => void, autoAdvanceInterval?: number) => {
     const [slideIndexState, setSlideIndexState] = useState(0);
     const slideIndex = slideIndexProp ?? slideIndexState;
+    const onSlideIndexChangeRef = useRef(onSlideIndexChange);
+    onSlideIndexChangeRef.current = onSlideIndexChange;
     const decrementSlideIndex = () => {
         if (!slides) {
             return;
@@ -14,13 +16,15 @@ export const useSlideIndex = (slides?: unknown[], slideIndexProp?: number, onSli
         setSlideIndexState(decrement(slides.length));
         onSlideIndexChange?.(decrement(slides.length)(slideIndex));
     };
-    const incrementSlideIndex = () => {
-        if (!slides) {
+    const incrementSlideIndex = useCallback(() => {
+        if (!slides?.length) {
             return;
         }
         setSlideIndexState(increment(slides.length));
         onSlideIndexChange?.(increment(slides.length)(slideIndex));
-    };
+    },
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        [slides?.length, slideIndex]);
     useTimeout(incrementSlideIndex, autoAdvanceInterval);
     return [slideIndex, decrementSlideIndex, incrementSlideIndex] as const;
 };
